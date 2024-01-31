4f83dfc5bf5b9ad9c9d8689f9584ceef62bbb57e
Shigeru Hagiwara <hgwrsgr@gmail.com>
2024-01-31 14:04:09 +0900

    move files to demo package

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.example.controller.MemoController;
+import com.example.demo.controller.MemoController;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = MemoController.class)
