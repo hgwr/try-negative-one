74ac521c6e892887e93c62e7b56c271afbcbcf78
Shigeru Hagiwara <hgwrsgr@gmail.com>
2024-01-31 16:36:06 +0900

    remove unused setting

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.example.demo.controller.MemoController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = MemoController.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
